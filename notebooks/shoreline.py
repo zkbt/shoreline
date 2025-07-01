127d3653e40ff5035689e80fd2981e7e20524d4e
Zach Berta-Thompson <zach.bertathompson@colorado.edu>
2025-07-01 07:46:01 -0600

    update notebooks

--- notebooks/shoreline.py
+++ notebooks/shoreline.py
@@ -108,123 +108,34 @@ def probability_of_atmosphere(
     return 1 / (1 + jnp.exp(distance_from_shoreline / width_of_shoreline))
 
 
-import jax.numpy as jnp
+def make_safe_for_latex(s):
+    s = clean(s).replace('0', 'o').replace('CO2', 'COO')
+    for k, v in dict(one=1, two=2, three=3, four=4, five=5, six=6, seven=7, eight=8, nine=9, zero=0).items():
+        s = s.replace(str(v), k)
+    return s
+    
+def latexify(posterior, label=''):
 
 
-def plot_shoreline_3D(pops=None, log_f_0=1.0, p=4.0, q=1.0, ln_w=0.0, **kw):
-    """ """
 
-    for kind in pops:
-        for k in pops[kind]:
-            pops[kind][k].annotate_planets = len(pops[kind][k]) < 20
-    # set up 2D grid for background colormap
-    log_v_1d = jnp.linspace(-4, 2, 1000)
-    log_f_1d = jnp.linspace(-5, 5, 1000)
-    log_v_2d, log_f_2d = jnp.meshgrid(log_v_1d, log_f_1d)
+    func_dict = {
+        "median": np.median,
+        "lower": lambda x: np.median(x) - np.percentile(x, 50-68.3/2), 
+        "upper": lambda x: np.percentile(x, 50+68.3/2) - np.median(x) }
 
-    N = 4
-    log_centers = np.linspace(0, -3, N)
-    log_widths = np.abs(np.gradient(log_centers))
-    lowers = 10 ** (log_centers - log_widths / 2) * u.Lsun
-    uppers = 10 ** (log_centers + log_widths / 2) * u.Lsun
-    centers = 10**log_centers * u.Lsun
-
-    fi = plt.figure(figsize=(12, 10), constrained_layout=True)
-    gs = plt.GridSpec(3, N, height_ratios=[1, 1, 1], figure=fi)
-
-    ax_stellar_radius = plt.subplot(gs[0, :])
-    plt.sca(ax_stellar_radius)
-    m = ErrorMap(
-        xaxis=StellarLuminosity(lim=[max(uppers), min(lowers)]),
-        yaxis=Radius(lim=[0.1, 30]),
-        ax=ax_stellar_radius,
-    )
-    for k in ["yes", "no"]:  # pops:
-        m.build(pops[k])
-    ax_stellar_radius.xaxis.set_ticks_position("top")
-    ax_stellar_radius.xaxis.set_label_position("top")
-
-    ax_stellar_flux = plt.subplot(gs[1, :])
-    plt.sca(ax_stellar_flux)
-    m = ErrorMap(
-        xaxis=StellarLuminosity(lim=[max(uppers), min(lowers)]),
-        yaxis=Flux(lim=[0.2e-4, 5e4]),
-        ax=ax_stellar_flux,
-    )
-    for k in ["yes", "no"]:  # pops:
-        m.build(pops[k])
-    # ax_stellar_flux.xaxis.set_ticks_position("top")
-    # ax_stellar_flux.xaxis.set_label_position("top")
-
-    ax_shoreline = None
-    for i in range(N):
-        ax_shoreline = plt.subplot(gs[-1, i], sharex=ax_shoreline, sharey=ax_shoreline)
-        m = ErrorMap(
-            xaxis=RelativeEscapeVelocity(lim=[1e-4, 100], kludge=True),
-            yaxis=Flux(lim=[0.2e-4, 5e4]),
-            ax=ax_shoreline,
-            size=100,
-        )
-
-        for c in ["yes", "no"]:  # pops
-            for k in pops[c]:
-                pop = pops[c][k]
-                allowed = (pop.stellar_luminosity() > lowers[i]) & (
-                    pop.stellar_luminosity() <= uppers[i]
-                )
-                if sum(allowed) > 0:
-                    subset = pop[allowed]
-                    subset._plotkw = pop._plotkw
-                    subset.label = pop.label
-                    if len(subset) < 10:
-                        subset.annotate_planets = True
-                    dots = subset[:]
-                    dots.bubble_anyway = True
-                    dots.s = 64
-                    # m.build(dots)
-                    m.build(subset)
-
-        log_P_2d = probability_of_atmosphere(
-            log_v=log_v_2d,
-            log_f=log_f_2d,
-            log_L=log_centers[i],
-            p=p,
-            q=q,
-            log_f_0=log_f_0,
-            ln_w=ln_w,
-        )
-        background = plt.pcolormesh(
-            10**log_v_2d,
-            10**log_f_2d,
-            log_P_2d,
-            cmap=one2another("burlywood", "lightskyblue"),
-            alpha=1,
-            zorder=-1e9,
-            rasterized=True,
-        )
-        shoreline_kw = dict(color="black", alpha=0.3)
-        shoreline_solar = plt.plot(
-            10**log_v_1d,
-            10 ** log_f_shoreline(log_f_0=log_f_0, p=p, q=q, log_v=log_v_1d, log_L=0),
-            linestyle="--",
-            **shoreline_kw,
-        )
-        shoreline_this = plt.plot(
-            10**log_v_1d,
-            10
-            ** log_f_shoreline(
-                log_f_0=log_f_0, p=p, q=q, log_v=log_v_1d, log_L=log_centers[i]
-            ),
-            linestyle=":",
-            **shoreline_kw,
-        )
-
-        # titled = plt.title(f'{log_P=:.2f}\n{log_f_0=:.2f}, {p=:.2f}, {q=:.2f}, {ln_w=:.2f}' )
-        # plt.colorbar()
-        plt.xscale("log")
-        plt.yscale("log")
-
-        plt.title(f'{centers[i].to_string(format="latex_inline")}')
-        if i > 0:
-            plt.setp(ax_shoreline.get_yticklabels(), visible=False)
-            plt.ylabel("")
+    summary = az.summary(posterior,  stat_funcs=func_dict) 
+    lines = []
+    for k in summary['median'].keys():
+        lower, upper = summary["lower"][k], summary["upper"][k] 
+        symmetric = np.abs(lower - upper)/(lower + upper) < 0.1
+        if symmetric:
+            sigma = (lower + upper)/2
+            s = f'{summary["median"][k]:.3g} \pm {sigma:.2g}'
+        else:
+            s = f'{summary["median"][k]:.3g}_{{-{lower:.2g}}}^{{+{upper:.2g}}}'
+        
+        lines.append(rf'\newcommand{{\{make_safe_for_latex(label)}{make_safe_for_latex(k)}}}{{{s}}}' + '\n')
+        s = f'{summary["median"][k]:.3g}'
+        lines.append(rf'\newcommand{{\{make_safe_for_latex(label)}{make_safe_for_latex(k)+'justvalue'}}}{{{s}}}' + '\n')
+    lines.append('\n')
+    return lines
